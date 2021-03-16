eaa33422b3367a8a5b6c096cd739523a6cb2652d
Federico <federico.peyrani.fp@gmail.com>
2021-03-16 18:30:40 +0100

    Fix: add `equals()` to `Track` so that the observers get updates only if the value actually changes

--- app/src/main/java/me/federicopeyrani/duetto/data/Track.kt
+++ app/src/main/java/me/federicopeyrani/duetto/data/Track.kt
@@ -4,13 +4,31 @@ import me.federicopeyrani.spotify_web_api.objects.ImageObject
 import me.federicopeyrani.spotify_web_api.objects.TrackObject
 
 fun TrackObject.toTrack() = Track(
+    id = id,
     title = name,
     artist = artists.joinToString(", ") { it.name },
     albumArtUrls = album.images
 )
 
 class Track(
+    val id: String,
     val title: String,
     val artist: String,
     val albumArtUrls: Array<ImageObject>,
-)
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Track
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
