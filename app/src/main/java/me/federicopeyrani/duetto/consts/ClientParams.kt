320b38ad6fe0674752ff5360e7b08cb6bba6d9f6
Federico <federico.peyrani.fp@gmail.com>
2021-03-24 11:57:51 +0100

    Change auth callback

--- app/src/main/java/me/federicopeyrani/duetto/consts/ClientParams.kt
+++ app/src/main/java/me/federicopeyrani/duetto/consts/ClientParams.kt
@@ -2,6 +2,6 @@ package me.federicopeyrani.duetto.consts
 
 object ClientParams {
     const val CLIENT_ID = "f11ce41c55004d0b95de68fa5d018a25"
-    const val REDIRECT_URI = "deify-login://callback"
+    const val REDIRECT_URI = "duetto-login://callback"
     val SCOPES = arrayOf("user-top-read", "user-read-playback-state", "user-read-recently-played")
 }
