c03000e8c0a54abe7442acbcb49cbf11a4e24c9f
Federico <federico.peyrani.fp@gmail.com>
2021-03-28 20:34:03 +0200

    Temporary fix: converting arrays

--- app/src/main/java/me/federicopeyrani/duetto/data/Converters.kt
+++ app/src/main/java/me/federicopeyrani/duetto/data/Converters.kt
@@ -6,6 +6,18 @@ import java.util.Date
 
 class Converters {
 
+    companion object {
+
+        private const val FIELD_SEPARATOR = "::"
+
+        private const val ELEMENTS_SEPARATOR = ",,"
+    }
+
+    private fun join(vararg args: Any) = args.joinToString(FIELD_SEPARATOR)
+
+    private fun <T> Array<T>.serialize(itemSerializer: (T) -> String) =
+        joinToString(ELEMENTS_SEPARATOR) { itemSerializer(it) }
+
     @TypeConverter
     fun fromTimestamp(value: Long?): Date? = value?.let { Date(it) }
 
@@ -14,14 +26,14 @@ class Converters {
 
     @TypeConverter
     fun fromImageObjectArray(imageObjectArray: Array<ImageObject>?): String? =
-        imageObjectArray?.joinToString(",") { "${it.height}::${it.width}::${it.url}" }
+        imageObjectArray?.serialize { join(it.height, it.width, it.url) }
 
     @TypeConverter
     fun stringToImageObjectArray(string: String?): Array<ImageObject> =
         if (string == null || string.isNullOrBlank()) {
             emptyArray()
         } else {
-            string.split(",").map {
+            string.split(",,").map {
                 val split = it.split("::")
                 ImageObject(split[0].toInt(), split[1].toInt(), split[2])
             }.toTypedArray()
@@ -29,14 +41,14 @@ class Converters {
 
     @TypeConverter
     fun fromArtistArray(imageObjectArray: Array<Artist>?): String? =
-        imageObjectArray?.joinToString(",") { "${it.id}::${it.name}" }
+        imageObjectArray?.serialize { join(it.id, it.name) }
 
     @TypeConverter
     fun stringToArtistArray(string: String?): Array<Artist> =
         if (string == null || string.isNullOrBlank()) {
             emptyArray()
         } else {
-            string.split(",").map {
+            string.split(",,").map {
                 val split = it.split("::")
                 Artist(split[0], split[1])
             }.toTypedArray()
