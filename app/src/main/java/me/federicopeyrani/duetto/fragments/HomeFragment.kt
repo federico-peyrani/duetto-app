e461866f4f3c91801e2a8e7feb98b2475e245e73
Federico <federico.peyrani.fp@gmail.com>
2021-03-30 15:04:57 +0200

    Add snackbar when loading data from the web fails

--- app/src/main/java/me/federicopeyrani/duetto/fragments/HomeFragment.kt
+++ app/src/main/java/me/federicopeyrani/duetto/fragments/HomeFragment.kt
@@ -7,8 +7,11 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.federicopeyrani.duetto.R
@@ -17,6 +20,7 @@ import me.federicopeyrani.duetto.adapters.TrackAdapter
 import me.federicopeyrani.duetto.databinding.FragmentHomeBinding
 import me.federicopeyrani.duetto.viewmodels.HomeViewModel
 import me.federicopeyrani.duetto.views.StackedBarsGraphView
+import java.io.IOException
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -24,6 +28,17 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
 
     private val viewModel: HomeViewModel by viewModels()
+
+    /**
+     * Handles exceptions raised while retrieving the data used to populate the cards on the screen.
+     */
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        val message = when (throwable) {
+            is IOException -> getString(R.string.error_network, throwable.localizedMessage)
+            else -> getString(R.string.error_generic)
+        }
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE).show()
+    }
 
     private suspend fun setTopGenres() {
         val genres = withContext(Dispatchers.IO) { viewModel.getTopGenres() }
@@ -61,9 +76,15 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         binding.topArtistsCard.rank.layoutManager = artistLayoutManager
 
         lifecycleScope.launchWhenCreated {
-            launch { setTopGenres() }
-            launch { setTopTracks(trackAdapter) }
-            launch { setTopArtists(artistAdapter) }
+            // Create a new children job with three children jobs of its own to be executed in
+            // parallel: passing Job() to the constructor ensures that if the new job fails,
+            // the parent job won't fail as well and exceptions won't be propagated, but at the same
+            // time ensures that if any of the three children job fails also the others will fail.
+            launch(Job() + coroutineExceptionHandler) {
+                launch { setTopGenres() }
+                launch { setTopTracks(trackAdapter) }
+                launch { setTopArtists(artistAdapter) }
+            }
         }
     }
 }
